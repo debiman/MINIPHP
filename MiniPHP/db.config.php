556fea694726d9bd9a5378e65a6545a242acf880
onlyzhoukai <672895146@qq.com>
2017-03-08 00:42:07 +0800

    Update db.config.php

--- MiniPHP/db.config.php
+++ MiniPHP/db.config.php
@@ -1,8 +1,7 @@
 <?php
 //数据库配置信息
-define("DB_USER","root");
-define("DB_PASS","zhoukai");
-define("DB_NAME","sjw_sms");
-define("DB_HOST","localhost");
+define("DB_USER","db_user");
+define("DB_PASS","db_pwd");
+define("DB_NAME","db");
+define("DB_HOST","db_host");
 define("DB_PORT",3306);
-//define("DB_DSN","mysql:host=127.0.0.1;port=3306;dbname=itfree");
